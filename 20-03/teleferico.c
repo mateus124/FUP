d282253d3f29247c125bc32b8bfb156044ae5009
mateus124 <alvesmateusq00@gmail.com>
2024-03-21 17:48:57 -0300

    teleferico

--- 20-03/teleferico.c
+++ 20-03/teleferico.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int max, alunos;
+    int max, alunos, viagens;
     scanf("%d %d", &max, &alunos);
+    max--;
+
+    viagens = alunos/max;
     
-    
+    if(alunos%max != 0) {
+        viagens++;
+    }
+
+    printf("%d", viagens);
 }
