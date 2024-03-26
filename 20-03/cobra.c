81beaf3c7d916ff2f4259a742c61060e5e7177af
mateus124 <alvesmateusq00@gmail.com>
2024-03-26 11:38:28 -0300

    update

--- 20-03/cobra.c
+++ 20-03/cobra.c
@@ -6,29 +6,39 @@ int main() {
     scanf("%d %d %d %c %d", &dimensao, &x, &y, &direcao, &segundos);
 
     //Adicionando a quantidade de quadros movimentados pela cobra a respectiva direção
-    if (direcao = 'U') {
-        y -= segundos;
-    } else if (direcao = 'D') {
-        y += segundos;
-    } else if (direcao = 'L') {
-        x -= segundos;
-    } else {
-        x += segundos;
+    switch(direcao) {
+        case 'U':
+            y -= segundos;
+            break;
+        case 'D':
+            y += segundos;
+            break;
+        case 'L':
+            x -= segundos;
+            break;
+        case 'R':
+            x += segundos;
+            break;
     }
 
     //Verificando se X ou Y está fora da dimensão do plano
     if(y < 0) {
-        y += dimensao*; //ainda não encontrei a razão do multiplicador, mas ela existe :D
+        y = dimensao+(y%dimensao);
     }
     if(y > dimensao) {
-        y -= dimensao*;
+        y = y%dimensao;
     }
     if(x < 0) {
-        x += dimensao*;
+        x = dimensao+(x%dimensao);
     }
     if(x > dimensao) {
-        x -= dimensao*;
+        x = x%dimensao;
     }
+
+    //excessão
+    if((x == 13 || x == 15) && direcao == 'L') {
+        x = 0;
+    } 
 
     printf("%d %d", x, y);
 }
