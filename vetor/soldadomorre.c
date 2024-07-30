26e35449a5cd3eab59e61b177abecb1421b59846
mateus124 <alvesmateusq00@gmail.com>
2024-07-30 19:31:21 -0300

    update

--- vetor/soldadomorre.c
+++ vetor/soldadomorre.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
 int main() {
+    int qtdSoldados, resultado = 0;
+    scanf("%d", &qtdSoldados);
     
+    int vetor[qtdSoldados];
+    for(int x = 0; x < qtdSoldados; x++) {
+        scanf("%d", &vetor[x]);
+    }
+
+    for(int i = 0; i < qtdSoldados; i++) {
+        if(vetor[i] != 1) {
+            if(i == 0 ) {
+                if(vetor[i+1] == 1) {
+                    resultado++;
+                }
+            } else if(i == qtdSoldados-1) {
+                if(vetor[i-1] == 1) {
+                    resultado++;
+                }
+            } else {
+                if(vetor[i-1] == 1 || vetor[i+1] == 1) {
+                    resultado++;
+                }
+            }
+        }
+    }
+    printf("%d", resultado);
 }
